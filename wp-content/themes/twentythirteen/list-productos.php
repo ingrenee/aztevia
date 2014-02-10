8e3a7ab431507f51cc234928a8088b7813fc4c01
renee morales <ing.renee.sis@gmail.com>
2014-02-10 00:14:45 -0500

    cdcdcdcdcdcdcd

--- wp-content/themes/twentythirteen/list-productos.php
+++ wp-content/themes/twentythirteen/list-productos.php
@@ -10,10 +10,27 @@
 <span class="system"> list-productoss.php</span>
 <!--  list-receta -->
 <article id="post-<?php the_ID(); ?>" <?php post_class('producto-item'); ?>>
+ 
+ 
+ 
   <div class="precios">
   <div class="precios-content">
-  <span class="descuento">Dscto. 80%</span>
-  <span class="precio">S/. 800.00</span>
+  <?PHP
+  $descuento=get_post_meta(get_the_ID(), 'wpcf-descuento', true);
+  ?>
+  <?PHP if($descuento):?>
+  <span class="descuento">Dscto. <?PHP echo $descuento;?>%</span>
+  <?PHP endif;?>
+  <?PHP
+  $precio=get_post_meta(get_the_ID(), 'wpcf-precio', true);
+  ?>
+  <?PHP if($precio):?>
+  <span class="precio color-font-1">S/. <?PHP 
+   echo $precio;
+  ?></span>
+  <?PHP else:?>
+  <span class="precio color-font-1"> Consultar </span>
+  <?PHP endif;?>
   </div>
   </div>
   
@@ -30,7 +47,7 @@
     <div class="texto">
       <?php // the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); 
 		
-		the_content_limit(200, "more")
+		the_content_limit(150, "more")
 		?>
     </div>
     
@@ -38,7 +55,7 @@
       <a href="<?php echo esc_url( twentythirteen_get_link_url() ); ?>" class="mas-informacion    color-font-1">M&aacute;s informaci&oacute;n ...</a>
       </div>
     
-    <div class="">
+    <div class="agregar-carrito-layer">
     <a href="#" class="agregar-carrito"><span>Agregar al carrito</span></a>
     </div>
     
